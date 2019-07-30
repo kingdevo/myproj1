4d1ae962da51ce2241cfb3f890cae110fc4d9ea5
kingdevo <devopsclasstest1@gmail.com>
2019-07-30 12:25:58 +0000

    code

--- demo.java
+++ demo.java
@@ -3,3 +3,11 @@ This is first line of this file
 ---------
 This is third edit of this file
 ---------
+
+--------
+This is fourth edit of this file
+-------
+
+--------
+This is 5th edit of this file
+-------
