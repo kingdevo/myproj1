b07c03e39fc487830654460882beefda6ddca198
kingdevo <devopsclasstest1@gmail.com>
2019-07-30 12:45:05 +0000

    code

--- demo1.java
+++ demo1.java
@@ -1,4 +1,14 @@
 This is the orignal file
+<<<<<<< Updated upstream
 ------
 This is the 2nd edit of this file
 ------
+=======
+---------
+This is the 1st edit of this file
+---------
+>>>>>>> Stashed changes
+
+--------------
+This is 3rd edit
+--------------
